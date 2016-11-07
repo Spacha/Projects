d0b14e5b9e91ff164f38fbc120998e79f8d073ff
Miika Sikala <miikasikala96@gmail.com>
2016-11-07 23:31:46 +0200

    Save and view(alpha) projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Project;
 use App\User;
 use Auth;
 
@@ -15,7 +16,10 @@ class ProjectController extends Controller
 
     public function index()
     {
-    	$projects = User::find(Auth::user()->id)->projects();
+        // Get all user's projects
+    	$projects = Auth::user()->projects()
+            ->orderBy('updated_at', 'desc')
+            ->get();
 
     	return view('home', [
     		'active' => 'projects',
@@ -23,8 +27,43 @@ class ProjectController extends Controller
     	]);
     }
 
+    public function view($id)
+    {
+        $project = Project::find($id);
+
+        return view('projects.view', [
+            'project' => $project
+        ]);
+    }
+
     public function save(Request $request)
     {
-        dd($request->input());
+        if (isset($request->id)) {
+
+            // Update existing projects
+            $project = Project::find($request->id);
+
+        } else {
+
+            // Create new project
+            $project = new Project();
+
+        }
+
+        $project->name = $request->name;
+        $project->category_id = $request->category;
+        $project->public = false;
+        $project->description = $request->description;
+        $project->url = $request->url;
+
+        $project->user_id = Auth::user()->id;
+
+        if ($project->save()) {
+            return redirect('projects/' . $project->id);
+        } else {
+            // TODO: Use flash!
+            return redirect('projects/create');
+        }
+
     }
 }
